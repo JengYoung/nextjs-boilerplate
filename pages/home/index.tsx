9b92decdb80d2513020c9faf56ab21241ce7a1fb
Jengyoung <young_pallete@naver.com>
2021-11-23 10:54:39 +0900

    chore: eslint, prettier setting
    1. eslint 문법 추가 (airbnb)
    2. prettier 충돌 방지
    3. prettier 기본 설정 추가

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,16 +1,11 @@
 import styled from '@emotion/styled';
-import React from 'react'
 
 const Message = styled.div`
   color: red;
 `;
 function index() {
   const message: string = 'HI';
-  return (
-    <Message css={{background: 'blue'}}>
-      {message}
-    </Message>
-  )
+  return <Message css={{ background: 'blue' }}>{message}</Message>;
 }
 
-export default index
+export default index;
