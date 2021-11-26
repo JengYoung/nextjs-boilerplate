e99635eeb9bd70327dcdc02c66e9a91f7c069305
Jengyoung <young_pallete@naver.com>
2021-11-27 00:41:45 +0900

    fix: stylelint not find syntax module error fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,9 +10,10 @@ const Message = styled.div`
 
 function index() {
   const message: string = 'HI';
+  console.log('hi');
   return (
     <>
-      <Message css={{ background: '#123142' }}>{message}</Message>
+      <Message css={{ background: 'blue' }}>{message}</Message>
       <Button width={96} height={32}>
         Click!
       </Button>
